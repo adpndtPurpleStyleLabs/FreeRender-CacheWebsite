20aa6ea0c8011190b3ddc3b11e0459a6098bf9cb
Aditya.Pandit <adpndt@gmail.com>
2024-12-29 13:56:50 +0530

    corrected selenium services

--- src/main/java/com/fastrender/CacheWebsite/Services/CacheWebsiteService.java
+++ src/main/java/com/fastrender/CacheWebsite/Services/CacheWebsiteService.java
@@ -2,26 +2,44 @@ package com.fastrender.CacheWebsite.Services;
 
 import org.openqa.selenium.WebDriver;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
+@Scope(value="prototype")
 public class CacheWebsiteService {
 
     @Autowired
-    private WebDriver webDriver;
+    private SeleniumService seleniumService;
 
     @Autowired
     private RedisTemplate<String, Object> redisTemplate;
 
+    private WebDriver driver;
+
     public void cacheWebsite(String url) {
-        long startTime = System.nanoTime(); // Record start time
-        webDriver.get(url);
-        String pageSource = webDriver.getPageSource();
-        redisTemplate.opsForValue().set(url, pageSource);
-        long endTime = System.nanoTime(); // Record end time
-        long durationInNano = endTime - startTime; // Execution time in nanoseconds
-        long durationInMillis = durationInNano / 1_000_000; // Convert to milliseconds
-        System.out.println("Execution time in milliseconds: " + durationInMillis);
+        try {
+            driver = seleniumService.getWebDriver();
+            long startTime = System.nanoTime();
+            driver.get(url);
+            String pageSource = driver.getPageSource();
+            redisTemplate.opsForValue().set(url, pageSource);
+            long endTime = System.nanoTime();
+            long durationInNano = endTime - startTime;
+            long durationInMillis = durationInNano / 1_000_000;
+            System.out.println("Execution time in milliseconds: " + durationInMillis);
+
+        } catch (Exception exception) {
+            if (null != driver) {
+                seleniumService.killWebDriver();
+            }
+            throw new RuntimeException("ERROR " + exception.getMessage() + exception.getStackTrace());
+
+        } finally {
+            if (null != driver) {
+                seleniumService.killWebDriver();
+            }
+        }
     }
 }
